256cd7a2f0493312dc4fa3c3dc5e65e243999797
Samir Jasarat <samirjasarat@gmail.com>
2019-03-21 15:06:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// add solution here
+var facts=["John Lennon was a member of the Beatles", "He hated the sound of his own voice"];
+var i;
+var newFacts=[];
+function johnLennonFacts(facts) {
+for(i=0;i<2;i++)
+{newFacts[i]=facts[i]+"!!!";
+return newFacts;}
+}
+
+console.log(johnLennonFacts(facts);)
